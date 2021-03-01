be61a52e1d592ff02f2a47b0789ce53ac5e7af29
OsricSystem <53537473+OsricSystem@users.noreply.github.com>
2021-03-01 01:07:22 -0600

    Update Headmate.java

--- Headmate.java
+++ Headmate.java
@@ -1,14 +1,16 @@
 public class Headmate{
-    String name, pronouns, subsystem, gender, age, bio;
-    String[] jobs;
-    
-    public Headmate(String name, String pronouns, String subsystem, String gender, String age, String bio, String... jobs){
-        this.name = name;
-        this.pronouns = pronouns;
-        this.subsystem = subsystem;
-        this.gender = gender;
-        this.age = age;
-        this.bio = bio;
-        this.jobs = jobs;
-    }
+	String name, pronouns, subsystem, gender, age, bio;
+	String[] jobs;
+	
+	public Headmate(String name, String pronouns, String subsystem, String gender, String age, String bio, String... jobs){
+		this.name = name;
+		this.pronouns = pronouns;
+		this.subsystem = subsystem;
+		this.gender = gender;
+		this.age = age;
+		this.bio = bio;
+		this.jobs = jobs;
+	}
+
+	private void write(String name, String... types){this.name, Main.write(types);}
 }
