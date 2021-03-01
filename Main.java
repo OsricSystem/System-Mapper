0c4caaceeda8be91bf40c2b3249b9d3288b8870c
OsricSystem <53537473+OsricSystem@users.noreply.github.com>
2021-03-01 01:03:22 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main{
+	File f;
+	
+	public static void main(String[] args){
+		try{
+			f = new File("data.dat");
+		}catch (IOException e){
+			break;
+		}
+		
+	}
+	
+	public static void write(String name, String... types){
+		FileWriter w = new FileWriter("data.dat");
+		if(types.length == 1 && types[0].euqals("newSystem")){
+			w.write(String.format("systemname:%s\ndesc:\nheadmates:", name));
+		}
+		w.close();
 	}
 }
