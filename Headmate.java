4fc0b908fc7468c0cb8edab6fee03f41512217c0
OsricSystem <53537473+OsricSystem@users.noreply.github.com>
2021-03-01 18:13:14 -0600

    prototype 2
    added getters, setters, and clear methods for all vars and a Headmate.toString()

--- Headmate.java
+++ Headmate.java
@@ -1,16 +1,56 @@
+package tester;
+
 public class Headmate{
-	String name, pronouns, subsystem, gender, age, bio;
-	String[] jobs;
-	
-	public Headmate(String name, String pronouns, String subsystem, String gender, String age, String bio, String... jobs){
-		this.name = name;
-		this.pronouns = pronouns;
-		this.subsystem = subsystem;
-		this.gender = gender;
-		this.age = age;
-		this.bio = bio;
-		this.jobs = jobs;
+	private String name, pronouns, subsystem, gender, age, desc;
+	private String[] jobs;
+
+	public Headmate(String name){ this.name=name; }
+
+	// GETTERS \\
+	public String getName(){ return name; }
+	public String getPronouns(){ return pronouns; }
+	public String getSubsystem(){ return subsystem; }
+	public String getGender(){ return gender; }
+	public String getAge(){ return age; }
+	public String getDesc(){ return desc; }
+	public String[] getJobs(){ return jobs; }
+
+	// SETTERS \\
+	public void setName(String name){ this.name=name; }
+	public void setPronouns(String pronouns){ this.pronouns=pronouns; }
+	public void setSubsystem(String subsystem){ this.subsystem=subsystem; }
+	public void setGender(String gender){ this.gender=gender; }
+	public void setAge(String age){ this.age=age; }
+	public void setDesc(String desc){ this.desc=desc; }
+	public void setJobs(String... jobs){ this.jobs=jobs; }
+	public void addJobs(String... jobs){ for(String job : jobs) addJob(job); }
+	public void addJob(String job){
+		String[] jobs=new String[this.jobs.length+1];
+		for(int i=0; i<this.jobs.length; i++) jobs[i]=this.jobs[i];
+		jobs[jobs.length-1]=job;
+		this.jobs=jobs;
 	}
 
-	private void write(String name, String... types){this.name, Main.write(types);}
+	// CLEAR \\
+	public void clearName(String name){ this.name=null; }
+	public void clearPronouns(String pronouns){ this.pronouns=null; }
+	public void clearSubsystem(String subsystem){ this.subsystem=null; }
+	public void clearGender(String gender){ this.gender=null; }
+	public void clearAge(String age){ this.age=null; }
+	public void clearDesc(String desc){ this.desc=null; }
+	public void clearJobs(String... jobs){ this.jobs=null; }
+
+	// other
+	public String toString(){
+		String out="";
+
+		out+="Name: "+ (name!=null? getName(): "");
+		out+="\nPronouns: "+ (pronouns!=null? getPronouns(): "");
+		out+="\nSubsystem: "+ (subsystem!=null? getSubsystem(): "");
+		out+="\nGender: "+ (gender!=null? getGender(): "");
+		out+="\nAge: "+ (age!=null? getAge(): "");
+		out+="\nBio: "+ (desc!=null? getDesc(): "");
+
+		return out;
+	}
 }
