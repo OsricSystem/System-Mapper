ab27481ee8b974cb02c531bf76f3d2611dea3732
noob247365 <noob247365@gmail.com>
2021-03-02 17:56:52 -0500

    Refactored with formatting XML

--- Main.java
+++ Main.java
@@ -8,87 +8,81 @@ import java.util.Scanner;
 import tester.models.Headmate;
 import tester.models.Sys;
 
-public class Main {
+public class Main{
 	private static Scanner in;
 	private static File file;
 	private static Sys system;
 
-	public static void main(String[] args) {
-		in = new Scanner(System.in);
-		file = new File("data.dat").getAbsoluteFile();
+	public static void main(String[] args){
+		in=new Scanner(System.in);
+		file=new File("data.dat").getAbsoluteFile();
 
-		try {
-			if (!file.exists() || file.length() == 0)
-				init();
-			else
-				setup();
+		try{
+			if(!file.exists() || file.length()==0) init();
+			else setup();
 
 			system.add(new Headmate("Tayto"));
 			// edit();
-		} catch (Exception e) {
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 
 		end();
 	}
 
-	private static void init() {
+	private static void init(){
 		System.out.println("Enter System Name:");
-		system = new Sys(in.nextLine());
+		system=new Sys(in.nextLine());
 
-		try (FileWriter w = new FileWriter(file.getPath())) {
-			w.write("System Name:" + system.getName());
-		} catch (IOException e) {
+		try(FileWriter w=new FileWriter(file.getPath())){
+			w.write("System Name:"+system.getName());
+		}catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	private static void setup() {
-		try (Scanner reader = new Scanner(file)) {
-			while (reader.hasNextLine()) {
-				String line = reader.nextLine();
-				int index = line.indexOf(':');
-				String label = line.substring(0, index);
-				String data = line.substring(index + 1);
-				if (label.equals("System Name"))
-					system = new Sys(data);
+	private static void setup(){
+		try(Scanner reader=new Scanner(file)){
+			while(reader.hasNextLine()){
+				String line=reader.nextLine();
+				int index=line.indexOf(':');
+				String label=line.substring(0, index);
+				String data=line.substring(index+1);
+				if(label.equals("System Name")) system=new Sys(data);
 			}
-		} catch (Exception e) {
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 
-	private static void edit() {
+	private static void edit(){
 		System.out.println("Entering Edit Mode\ntype 'EXIT' to quit");
-		while (true) {
-			String inp = in.nextLine();
-			if (inp.equals("EXIT"))
-				break;
+		while(true){
+			String inp=in.nextLine();
+			if(inp.equals("EXIT")) break;
 		}
 	}
 
-	private static void end() {
-		if (!file.exists()) {
+	private static void end(){
+		if(!file.exists()){
 			System.out.println("Unable to read generated file");
 			return;
 		}
 
-		try (Scanner out = new Scanner(file)) {
-			while (out.hasNextLine())
-				System.out.println(out.nextLine());
-		} catch (IOException e) {
+		try(Scanner out=new Scanner(file)){
+			while(out.hasNextLine()) System.out.println(out.nextLine());
+		}catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	private static void write(String name, String... types) {
-		try {
-			FileWriter w = new FileWriter("data.dat");
-			if (types.length == 1 && types[0].equals("newSystem")) {
+	private static void write(String name, String... types){
+		try{
+			FileWriter w=new FileWriter("data.dat");
+			if(types.length==1 && types[0].equals("newSystem")){
 				w.write(String.format("systemname:%s\ndesc:\nheadmates:", name));
 			}
 			w.close();
-		} catch (IOException e) {
-		}
+		}catch(IOException e){}
 	}
 }
--- models/Headmate.java
+++ models/Headmate.java
@@ -6,53 +6,53 @@ public class Headmate{
 	private String name, pronouns, subsystem, gender, age, desc;
 	private final LinkedList<String> jobs;
 
-	public Headmate(String name) {
-		this.name = name;
-		jobs = new LinkedList<String>();
+	public Headmate(String name){
+		this.name=name;
+		jobs=new LinkedList<String>();
 	}
 
 	// GETTERS \\
-	public String getName() { return name; }
-	public String getPronouns() { return pronouns; }
-	public String getSubsystem() { return subsystem; }
-	public String getGender() { return gender; }
-	public String getAge() { return age; }
-	public String getDesc() { return desc; }
-	public String[] getJobs() { return jobs.toArray(new String[jobs.size()]); }
+	public String getName(){ return name; }
+	public String getPronouns(){ return pronouns; }
+	public String getSubsystem(){ return subsystem; }
+	public String getGender(){ return gender; }
+	public String getAge(){ return age; }
+	public String getDesc(){ return desc; }
+	public String[] getJobs(){ return jobs.toArray(new String[jobs.size()]); }
 
 	// SETTERS \\
-	public void setName(String name) { this.name = name; }
-	public void setPronouns(String pronouns) { this.pronouns = pronouns; }
-	public void setSubsystem(String subsystem) { this.subsystem = subsystem; }
-	public void setGender(String gender) { this.gender = gender; }
-	public void setAge(String age) { this.age = age; }
-	public void setDesc(String desc) { this.desc = desc; }
-	public void setJobs(String... jobs) {
+	public void setName(String name){ this.name=name; }
+	public void setPronouns(String pronouns){ this.pronouns=pronouns; }
+	public void setSubsystem(String subsystem){ this.subsystem=subsystem; }
+	public void setGender(String gender){ this.gender=gender; }
+	public void setAge(String age){ this.age=age; }
+	public void setDesc(String desc){ this.desc=desc; }
+	public void setJobs(String... jobs){
 		clearJobs();
 		addJobs(jobs);
 	}
-	public void addJobs(String... jobs) { for(String job : jobs) addJob(job); }
-	public void addJob(String job) { this.jobs.add(job); }
+	public void addJobs(String... jobs){ for(String job : jobs) addJob(job); }
+	public void addJob(String job){ this.jobs.add(job); }
 
 	// CLEAR \\
-	public void clearName() { this.name = null; }
-	public void clearPronouns() { this.pronouns = null; }
-	public void clearSubsystem() { this.subsystem = null; }
-	public void clearGender() { this.gender = null; }
-	public void clearAge() { this.age = null; }
-	public void clearDesc() { this.desc = null; }
-	public void clearJobs() { this.jobs.clear(); }
+	public void clearName(){ this.name=null; }
+	public void clearPronouns(){ this.pronouns=null; }
+	public void clearSubsystem(){ this.subsystem=null; }
+	public void clearGender(){ this.gender=null; }
+	public void clearAge(){ this.age=null; }
+	public void clearDesc(){ this.desc=null; }
+	public void clearJobs(){ this.jobs.clear(); }
 
 	// other
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
+	public String toString(){
+		StringBuilder sb=new StringBuilder();
 
-		sb.append("Name: " + (name != null ? getName() : ""));
-		sb.append("\nPronouns: " + (pronouns != null ? getPronouns() : ""));
-		sb.append("\nSubsystem: " + (subsystem != null ? getSubsystem() : ""));
-		sb.append("\nGender: " + (gender != null ? getGender() : ""));
-		sb.append("\nAge: " + (age != null ? getAge() : ""));
-		sb.append("\nBio: " + (desc != null ? getDesc() : ""));
+		sb.append("Name: "+ (name!=null? getName(): ""));
+		sb.append("\nPronouns: "+ (pronouns!=null? getPronouns(): ""));
+		sb.append("\nSubsystem: "+ (subsystem!=null? getSubsystem(): ""));
+		sb.append("\nGender: "+ (gender!=null? getGender(): ""));
+		sb.append("\nAge: "+ (age!=null? getAge(): ""));
+		sb.append("\nBio: "+ (desc!=null? getDesc(): ""));
 
 		return sb.toString();
 	}
